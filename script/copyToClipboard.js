ccc150a33510423deb6d759762b1d7db989c8469
Raushan <raurajar1@gmail.com>
2018-12-26 16:27:02 +0530

    Copy for elements other than input or textarea

--- script/copyToClipboard.js
+++ script/copyToClipboard.js
@@ -1,6 +1,20 @@
-function copy() {
-    var copyText = document.getElementById("textToCopy");
-    copyText.select();
-    document.execCommand("copy");
-    alert("Copied text:\n"+copyText.value)
-  } 
+function copyIn() {
+  var copyText = document.getElementById("textToCopy");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function copy(element) {
+  var v = document.getElementById(element);
+  var newInput = document.createElement('input');
+  newInput.value = v.lastChild.data;
+  newInput.id = "cp";
+  newInput.setAttribute('readonly', '');
+  newInput.style.position = 'absolute';
+  newInput.style.left = '-9999px';
+  v.parentNode.insertBefore(newInput, v.nextSibling);
+  newInput.select();
+  document.execCommand("copy");
+  newInput.parentNode.removeChild(newInput);
+}
+
